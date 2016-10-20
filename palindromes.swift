24db7a2e605a846f809a4d97dbec6ada867923fd
Stephanholly <sholly@usc.edu>
2016-10-20 11:50:56 -0700

    first commit

---
+++ palindromes.swift
@@ -0,0 +1,3 @@
+// Create a function that checks to see if a word is a palindrome (i.e. a word that is spelled the same forward and backward. eg, racecar, taco cat, mom)
+
+// HINT: you are going to have to implement built in string methods to do this. Remeber Google is your friend!
